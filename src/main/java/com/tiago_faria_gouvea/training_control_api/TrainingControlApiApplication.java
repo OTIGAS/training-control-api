47ce089b18356a5be539785149c7f4b7b80cae4e
OTIGAS <tfariagouvea@gmail.com>
2025-08-04 10:16:52 -0300

    first commit

---
+++ src/main/java/com/tiago_faria_gouvea/training_control_api/TrainingControlApiApplication.java
@@ -0,0 +1,13 @@
+package com.tiago_faria_gouvea.training_control_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago_faria_gouvea/training_control_api/TrainingControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago_faria_gouvea.training_control_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
